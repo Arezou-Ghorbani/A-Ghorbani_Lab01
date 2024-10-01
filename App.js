4adb44f237cbd2e3257878f3de180ea26e1322ce
Arezou-Ghorbani <ArezouGhorbaniii@gmail.com>
2024-09-30 23:41:23 -0400

    Tasks UI's Designed and impelemented

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
-{/* Today's Tasks */}
 
-<View style={styles.tasksWrapper}></View>
-<Text style={styles.sectionTitle}> Today's Tasks </Text>
-<View style={styles.items}></View>
-{/* Task should be shown here */}
+      {/* Today's Tasks */}
+
+      <View style={styles.tasksWrapper}></View>
+      <Text style={styles.sectionTitle}> Today's Tasks </Text>
+      <View style={styles.items}></View>
+      {/* Task should be shown here */}
+      <Task text={'task1'} />
+      <Task text={'task2'} />
+
     </View>
   );
 }
@@ -19,15 +22,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#EBEAED',
-   
+
   },
-  tasksWrapper:{
-    paddingTop:80,
-    paddingHorizontal:20,
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
   },
-  sectionTitle:{
-    fontSize:24,
-    fontWeight:'bold'
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
   },
-  items:{},
+  items: {
+    marginTop:30
+  },
 });
